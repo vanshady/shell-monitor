c504bde61535b503991228be469a74c87957c9e8
Minwei Xu <faceswilliam@gmail.com>
2017-02-07 00:09:47 -0500

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Shell Monitor
+
+This is a configurable real-time shell monitor.
+
+## Usage
+
+```{}
+npm install
+npm start
+```
+
+## Configuration
+
+1. command
+
+1. color
+
+1. time interval
---
+++ index.js
@@ -0,0 +1,20 @@
+require('shelljs/global');
+
+const Jetty = require("jetty");
+const jetty = new Jetty(process.stdout);
+
+const command = '';
+const timeout = 2000;
+const color = 1; // -1: white, 0: black, 1: red, 2: green, 3: yellow, 4: blue
+jetty.clear();
+
+(function runCommand() {
+    exec(command, function(code, stdout, stderr) {
+            if (code != 0) return; // break if error
+            jetty.clear();
+            jetty.moveTo([0, 0]);
+            jetty.rgb(color);
+            jetty.text(stdout);
+            setTimeout(runCommand, timeout);
+        });
+}()) // this is a closure
